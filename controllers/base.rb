48137061636874abbc23e517eb93ff8a880fa57f
edsong1257 <admin98@gmail.com>
2016-05-15 12:18:23 +0800

    modifying views

--- controllers/base.rb
+++ controllers/base.rb
@@ -5,6 +5,7 @@ class MessengerApp < Sinatra::Base
   use Rack::Session::Cookie, expire_after: 2_592_000 # One month in seconds
 
   set :views, File.expand_path('../../views', __FILE__)
+  set :public_dir, File.expand_path('../../public', __FILE__)
 
   before do
     @current_account = session[:current_account]
