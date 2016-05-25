dc8a582b735bbaa97b034ea87ba4e175e512a1ab
conkytw <chenky0916@gmail.com>
2016-05-25 18:13:42 +0800

    add message related file

---
+++ services/get_all_messages.rb
@@ -0,0 +1,23 @@
+require 'http'
+
+# Returns an authenticated user, or nil
+class GetAllMessages
+  def self.call(username: , auth_token:)
+    response = HTTP.auth("Bearer #{auth_token}")
+                    .get("#{ENV['API_HOST']}/api/v1/account/#{username}/messages/",
+
+    response.code == 200 ? extract_message(response.parse) : nil
+  end
+
+  def self.extract_message(messages)
+    messages['data'].map do |mes|
+      {
+        id: mes['id']
+        sender: mes['data']['sender']
+        receiver: mes['data']['receiver']
+        message: mes['data']['message_base64']
+        time: mes['data']['time']
+      }
+    end
+  end
+end
