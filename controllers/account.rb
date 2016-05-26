3fc84a2bd6cda9a068c641ed260a1945dda8879e
conkytw <chenky0916@gmail.com>
2016-05-26 16:48:27 +0800

    modified message and show token on account

--- controllers/account.rb
+++ controllers/account.rb
@@ -38,6 +38,7 @@ class MessengerApp < Sinatra::Base
 
   get_user = lambda do
     if @current_account && @current_account['username'] == params[:username]
+      @auth_token = session[:auth_token]
       slim(:account)
     else
       slim(:login)
