cc83e4b18c795a5077e98d222eb22ac38444e1df
pengyuchen <pengyu@nlplab.cc>
2016-05-29 14:46:13 +0800

    connect all chatroom with api

--- controllers/message.rb
+++ controllers/message.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
+require 'slim'
+require 'http'
 
 # Base class for ConfigShare Web Application
 class MessengerApp < Sinatra::Base
-  get_message = lambda do
+  get_all_message = lambda do
     if @current_account && @current_account['username'] == params[:username]
       @messages = GetAllMessages.call(username: params[:username],
                                       auth_token: session[:auth_token])
@@ -12,8 +14,9 @@ class MessengerApp < Sinatra::Base
 
   get_chatroom = lambda do
     if @current_account
-      @messages = GetAllMessages.call(username: params[:username],
-                                      auth_token: session[:auth_token])
+      response = HTTP.auth("Bearer #{session[:auth_token]}")
+                       .get("#{ENV['API_HOST']}/api/v1/account/")
+      @all_users = response.parse
       slim :chatroom
     else
       flash[:notice] = "you must login before you chat with others"
@@ -21,6 +24,12 @@ class MessengerApp < Sinatra::Base
     end
   end
 
-  get '/message/:username/?', &get_message
+  get_message = lambda do
+    Slim::Template.new('views/message.slim').render(Object.new)
+  end
+
+  get '/message/:username/?', &get_all_message
   get '/message/:username/chatroom/?', &get_chatroom
+  get '/message/:sender/chatroom/:receiver/?', &get_message
+
 end
