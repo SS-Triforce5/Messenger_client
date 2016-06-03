221142ea7483da0503f3f2f526953d47dc42314c
edsong1257 <admin98@gmail.com>
2016-06-03 08:29:37 +0800

    github signin beta

--- controllers/account.rb
+++ controllers/account.rb
@@ -1,40 +1,7 @@
 require 'sinatra'
 
-# Base class for ConfigShare Web Application
+# Account resource routes
 class MessengerApp < Sinatra::Base
-  get_login = lambda do
-    slim :login
-  end
-
-  post_login = lambda do
-    credentials = LoginCredentials.call(params)
-
-    if credentials.failure?
-      flash[:error] = 'Please enter both your username and password'
-      redirect '/login'
-      halt
-    end
-
-    auth_account = FindAuthenticatedAccount.call(credentials)
-
-    if auth_account
-      @current_account =auth_account['account']
-      session[:auth_token] = auth_account['auth_token']
-      session[:current_account] = SecureMessage.encrypt(@current_account)
-      flash[:notice] = "Welcome back #{@current_account['username']}"
-      redirect '/'
-    else
-      flash[:error] = 'Your username or password did not match our records'
-      slim :login
-    end
-  end
-
-  get_logout = lambda do
-    @current_account = nil
-    session[:current_account] = nil
-    flash[:notice] = 'You have logged out - please login again to use this site'
-    slim :login
-  end
 
   get_user = lambda do
     if @current_account && @current_account['username'] == params[:username]
@@ -45,8 +12,6 @@ class MessengerApp < Sinatra::Base
     end
   end
 
-  get '/login/?', &get_login
-  post '/login/?', &post_login
-  get '/logout/?', &get_logout
+
   get '/account/:username', &get_user
 end
---
+++ controllers/authentication.rb
@@ -0,0 +1,64 @@
+require 'sinatra'
+
+# Account Authentication, Registration, and Authorization
+class MessengerApp < Sinatra::Base
+
+get_login = lambda do
+  @gh_url = HTTP.get("#{ENV['API_HOST']}/github_sso_url").parse['url']
+  slim :login
+end
+
+post_login = lambda do
+  credentials = LoginCredentials.call(params)
+
+  if credentials.failure?
+    flash[:error] = 'Please enter both your username and password'
+    redirect '/login'
+    halt
+  end
+
+  auth_account = FindAuthenticatedAccount.call(credentials)
+
+  if auth_account
+    @current_account =auth_account['account']
+    session[:auth_token] = auth_account['auth_token']
+    session[:current_account] = SecureMessage.encrypt(@current_account)
+    flash[:notice] = "Welcome back #{@current_account['username']}"
+    redirect '/'
+  else
+    flash[:error] = 'Your username or password did not match our records'
+    slim :login
+  end
+end
+
+get_logout = lambda do
+  @current_account = nil
+  session[:current_account] = nil
+  flash[:notice] = 'You have logged out - please login again to use this site'
+  slim :login
+end
+
+get_github_callback do
+  begin
+    sso_account = RetrieveGithubAccount.call(params['code'])
+
+    # TODO: DRY out following lines (also in /login controller)
+    @current_account = sso_account['account']
+    session[:auth_token] = sso_account['auth_token']
+    session[:current_account] = SecureMessage.encrypt(@current_account)
+    flash[:notice] = "Welcome back #{@current_account['username']}"
+    redirect '/'
+
+  rescue => e
+    flash[:error] = 'Could not sign in using Github'
+    puts "RESCUE: #{e}"
+    redirect '/login'
+  end
+end
+
+get '/login/?', &get_login
+post '/login/?', &post_login
+get '/logout/?', &get_logout
+get '/github_callback/?', &get_github_callback
+
+end
